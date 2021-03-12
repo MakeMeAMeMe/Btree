07320cb471f15e3a31be91da23b7f6864559220a
Gui-g <guilira94@gmail.com>
2021-03-12 17:24:31 -0300

    btree

--- main.c
+++ main.c
@@ -1,12 +1,18 @@
 #include "btree.h"
+#include <time.h>
 
 int main(int argc, char const *argv[])
 {
     btree** btree = create_tree(1);
-    
+    clock_t time;
+
+    time = clock();    
     for(int i = 0; i < 100; i++) {
         insert_key(btree, i);
     }
-
+    time = clock() - time;
+    double time_taken = ((double) time)/ CLOCKS_PER_SEC;
+    
+    printf("%f\n", time_taken);
     return 0;
 }
