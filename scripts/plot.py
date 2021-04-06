0d7d656968466860d1ab80cf4c9905b7e68460f5
FQA <otavioalmeida650@protonmail.ch>
2021-04-06 16:59:14 -0300

    plot nice

--- scripts/plot.py
+++ scripts/plot.py
@@ -3,9 +3,13 @@ from pathlib import Path
 from typing import Dict
 
 
-def plt(pior_caso_data: Dict[int, float], medio_caso_data: Dict[int, float], tree_name: str):
-    figure = pyplot.figure()
-    figure.suptitle(f"Tempo Execução {tree_name}")
+def plt(
+        pior_caso_data_avl: Dict[int, float],
+        medio_caso_data_avl: Dict[int, float],
+        pior_caso_data_btree: Dict[int, float],
+        medio_caso_data_btree: Dict[int, float]):
+    figure = pyplot.figure(figsize=(15,10), dpi=80)
+    figure.suptitle(f"Tempo Execução - BTree e AVL")
 
     subplot_pior_caso = figure.add_subplot(211)
     subplot_medio_caso = figure.add_subplot(212)
@@ -18,12 +22,31 @@ def plt(pior_caso_data: Dict[int, float], medio_caso_data: Dict[int, float], tre
     subplot_medio_caso.set_xlabel("Qtd Chaves (unit)")
     subplot_medio_caso.set_ylabel("Tempo Exucução (s)")
 
-    subplot_pior_caso.plot(pior_caso_data.keys(), pior_caso_data.values())
-    subplot_medio_caso.plot(medio_caso_data.keys(), medio_caso_data.values())
+    subplot_pior_caso.plot(
+        pior_caso_data_avl.keys(),
+        pior_caso_data_avl.values(),
+        label="AVL")
+    subplot_pior_caso.plot(
+        pior_caso_data_btree.keys(),
+        pior_caso_data_btree.values(),
+        label="BTree")
 
-    pyplot.tight_layout()
+    subplot_medio_caso.plot(
+        medio_caso_data_avl.keys(),
+        medio_caso_data_avl.values(),
+        label="AVL")
+    subplot_medio_caso.plot(
+        medio_caso_data_btree.keys(),
+        medio_caso_data_btree.values(),
+        label="BTree")
 
-    pyplot.savefig(f"./data/{tree_name}_exec_time.png")
+    # pyplot.tight_layout()
+    
+    subplot_pior_caso.legend()
+    subplot_medio_caso.legend()
+    
+    pyplot.savefig(f"./data/avl_btree_exec_time.svg")
+    pyplot.savefig(f"./data/avl_btree_exec_time.png")
 
 
 def get_data(path: Path) -> Dict[int, float]:
@@ -48,5 +71,9 @@ medio_caso_avl_data = get_data(medio_caso_avl_path)
 pior_caso_btree_data = get_data(pior_caso_btree_path)
 medio_caso_btree_data = get_data(medio_caso_btree_path)
 
-plt(pior_caso_data=pior_caso_btree_data, medio_caso_data=medio_caso_btree_data, tree_name="BTree")
-plt(pior_caso_data=pior_caso_avl_data, medio_caso_data=medio_caso_avl_data, tree_name="AVL")
+plt(
+    pior_caso_data_avl=pior_caso_avl_data,
+    medio_caso_data_avl=medio_caso_avl_data,
+    pior_caso_data_btree=pior_caso_btree_data,
+    medio_caso_data_btree=medio_caso_btree_data
+)
